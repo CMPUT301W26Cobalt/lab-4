7ff9183cf97fa5d792884ee9ab5b82f5092d3213
Alishba Nosheen <anosheen@ualberta.ca>
2026-01-30 14:49:20 -0700

    Add Star class

---
+++ app/src/main/java/com/example/lab4/Star.java
@@ -0,0 +1,10 @@
+package com.example.lab4;
+
+public class Star extends Shape {
+    private int points;
+
+    public Star(int x, int y, int points) {
+        super(x, y);
+        this.points = points;
+    }
+}
